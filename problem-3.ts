3215502573f82030cb3f89f7ef0b8952a3d427ea
Sk Shihab <90505012+Skshihabbd@users.noreply.github.com>
2024-11-10 17:57:14 +0600

    Update problem-3.ts

--- problem-3.ts
+++ problem-3.ts
@@ -1,13 +1,13 @@
 {
   const sentence =
-    "I love typescript  typescript typescript typescript   typescript";
-  const word = "typescript";
+    "I love typescript  typescriPt typescript typescript   typescript";
+  const word = "typescripT";
 
   function countWordOccurrences(param1: string, param2: string): number {
     const newArray = param1.split(" ");
-    const count = newArray.filter((item) => item === param2).length;
+    const count = newArray.filter((item) => item.toLowerCase() === param2.toLowerCase()).length;
     return count;
   }
 
   const result = countWordOccurrences(sentence, word);
-}
+console.log(result)}
